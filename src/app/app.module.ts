ebd68380ae148308fe2fc7f4e43bd7710e0e0722
Franco Oliva <franco.oliva1991@gmail.com>
2021-06-10 14:28:06 -0300

    s10 v143, ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
@@ -7,7 +7,12 @@ import { AppComponent } from './app.component';
 // Módulo personalizado
 import { SharedModule } from './shared/shared.module';
 
+import { VentasModule } from './ventas/ventas.module';
 
+// Cambiar el locale de la app
+import localeEs from '@angular/common/locales/es-AR'
+import { registerLocaleData } from '@angular/common';
+registerLocaleData( localeEs );
 
 
 
@@ -20,9 +25,12 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     AppRouterModule,
     BrowserModule,
-    SharedModule
+    SharedModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-AR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
