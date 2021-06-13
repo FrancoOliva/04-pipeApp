dacd77917e056850c28f7c57a1350e041d3e668d
Franco Oliva <franco.oliva1991@gmail.com>
2021-06-13 14:29:57 -0300

    completamos tabla, creamos interface v160

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interface/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,14 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent{
 
   esMayusculas: boolean = false;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
 
-  constructor() { }
+  // constructor() { }
 
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void {
+  // }
 
   cambiarNosotros(){
     
